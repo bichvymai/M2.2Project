85178bad26ecbe31a4115177beb59f8bc7f146dc
bichvymai <bichvymai@gmail.com>
2022-05-04 18:28:15 +0700

    add files

---
+++ src/components/CharacterItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CharacterItem(name, images) {
+  return (
+    <li class="char-type">
+      <div className="char-type-title">
+        <span>Body</span>
+      </div>
+      <div className="char-list">
+        <img src="" alt="" />
+      </div>
+    </li>
+  );
+}
+
+export default CharacterItem;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="game-title">
+      <div className="char-title">
+        <h1>CHARACTER</h1>
+      </div>
+      <div className="char-title">
+        <span>CUSTOMIZATION</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
